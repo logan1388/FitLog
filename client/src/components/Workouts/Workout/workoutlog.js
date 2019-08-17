1325958d46f8eb0c3b360e01d9c22cde18127b27
shiv raj <shiv1988@gmail.com>
2019-08-17 17:24:42 -0400

    workoutlog initial commit

--- client/src/components/Workouts/Workout/workoutlog.js
+++ client/src/components/Workouts/Workout/workoutlog.js
@@ -4,12 +4,25 @@ import {Collapse} from 'react-collapse';
 const Workoutlog = (props) => {
     return (
         <Fragment>
-            <div className='ExerciseContainer' onClick={props.clicked}>
-                    <span>{props.children}</span>
+            <div className='ExerciseContainer'>
+                    <span onClick={props.clicked}>{props.children}</span>
                     <Collapse isOpened={props.isOpened}>
                         <div className='ExpExerciseContainer'>
-                            <div>Weight</div>
-                            <div>Count</div>
+                            <div className='DetailsLogContainer'>
+                                <span>Weight</span>                            
+                                <input type='number'/>
+                                <select>
+                                    <option value='1'>lbs</option>
+                                    <option value='2'>kgs</option>
+                                </select>
+                            </div>
+                            <div className='DetailsLogContainer'>
+                                <span>Count</span>
+                                <input type='number'/>
+                            </div>               
+                            <div id="SaveLogBtn">
+                                <button type="button" className="btn btn-outline-dark">Add</button>
+                            </div>
                         </div>
                     </Collapse>             
                 </div>
